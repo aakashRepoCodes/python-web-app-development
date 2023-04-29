8153db8e75bef4f913760f8d6aeca14f53ea6b7f
Aakash <aakashchaturvedi7@gamil.com>
2023-04-29 21:26:26 +0200

    Added template inheritance

--- base/views.py
+++ base/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {id:1, "name": 'Python Chatroom'},
+    {id:1, "name": 'JavaScript Chatroom'}, 
+    {id:1, "name": 'Ruby Chatroom'}, 
+    {id:1, "name": 'Java Chatroom'},
+]
+
 def home(request):
-    return render(request=request, template_name="home.html")
+    return render(request=request, template_name="base/home.html")
     #return HttpResponse('Home Page')
 
 
 def rooms(request):
-    return render(request, "room.html")
+    return render(request, "base/room.html")
     #return HttpResponse('Rooms')
