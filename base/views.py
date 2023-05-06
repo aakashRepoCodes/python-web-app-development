cb552d403b5e4621c764a787dba1cf1d5af89a09
Aakash <aakashchaturvedi7@gamil.com>
2023-05-06 19:29:01 +0200

    Added Search & Authentication

--- base/views.py
+++ base/views.py
@@ -1,61 +1,100 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from django.contrib.auth import authenticate, login
+from .models import Room, Topic, User
 from .forms import RoomForm
 
 # Create your views here.
 
 chatrooms = [
-    {id:1, 'name': 'Python Chatroom'},
-    {id:2, 'name': 'JavaScript Chatroom'}, 
-    {id:3, 'name': 'Ruby Chatroom'}, 
+    {id: 1, "name": "Python Chatroom"},
+    {id: 2, "name": "JavaScript Chatroom"},
+    {id: 3, "name": "Ruby Chatroom"},
 ]
 
 
 def home(request):
-    chatrooms = Room.objects.all
-    return render(request,"base/home.html",{'rooms': chatrooms})
-    #return HttpResponse('Home Page')
+    queryParam = request.GET.get("q") if request.GET.get("q") != None else ""
+    topics = Topic.objects.all()
+
+    chatrooms = Room.objects.filter(
+        Q(topic__name__icontains=queryParam)
+        | Q(name__icontains=queryParam)
+        | Q(description__icontains=queryParam)
+    )
+
+    room_count = chatrooms.count()
+
+    return render(
+        request,
+        "base/home.html",
+        {"rooms": chatrooms, "topics": topics, "room_count": room_count},
+    )
+    #    return HttpResponse('Home Page')
 
 
 def rooms(request, pk):
-    room = Room.objects.get(id = pk)
-    context = {'room': room}
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
     return render(request, "base/room.html", context)
-    #return HttpResponse('Rooms')
+    # return HttpResponse('Rooms')
 
 
 def create_room(request):
     form = RoomForm()
-    context = {'form': form}
+    context = {"form": form}
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('home')
-    return render(request, 'base/room_form.html', context)
+        return redirect("home")
+    return render(request, "base/room_form.html", context)
+
 
 def update_room(request, pk):
-    room = Room.objects.get(id = pk)
-    form = RoomForm(instance = room)
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
 
     if request.method == "POST":
-        form = RoomForm(request.POST , instance=room )
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
-        return redirect('home')
+        return redirect("home")
 
-    context = {'form': form}
-    return render(request, 'base/room_form.html', context)
+    context = {"form": form}
+    return render(request, "base/room_form.html", context)
+
 
 def delete_room(request, pk):
-    room = Room.objects.get(id = pk)
+    room = Room.objects.get(id=pk)
 
-    if request.method == 'POST': 
+    if request.method == "POST":
         room.delete()
-        return redirect('home')
-         
-    context = {'obj': room}
+        return redirect("home")
 
-    return render(request, 'base/delete_room.html', context)
+    context = {"obj": room}
+
+    return render(request, "base/delete_room.html", context)
+
+
+def signin_signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print("no such user")
+
+        user = authenticate(request=request, username=username, password=password)
+
+        if user is not None:
+            print("Login sucesss...")
+            return redirect("home")
+        else:
+            print("Login failed")
+
+    return render(request, "base/signin_signup.html", {})
